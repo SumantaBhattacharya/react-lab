bd6bbeee80ec54834cae81f512e1ff945c8c2fbf
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-28 19:20:43 +0530

    feat: implement Header component with dynamic navigation based on authentication status

--- MegaBlog/src/components/Header/Header.jsx
+++ MegaBlog/src/components/Header/Header.jsx
@@ -1,10 +1,74 @@
 import React from 'react'
+import { Container, Logo, LogoutBtn } from '../index'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
-const Header = () => {
+function Header() {
+  const authStatus = useSelector((state) => state.auth.status)// C:\Users\Sumanta Bhattacharya\OneDrive\Documents\Desktop\React\MegaBlog\src\features\auth\authSlice.js
+  const navigate = useNavigate()
+
+  const navItems = [
+    {
+      name: 'Home',
+      slug: "/",
+      active: true
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ]
+
+
   return (
-    <div>
-      header
-    </div>
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo width='70px' />
+            </Link>
+          </div>
+          <ul className='flex ml-auto'>
+            {navItems.map((item) =>
+              item.active ? (
+                // if active then show the item
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}// path to navigate to
+                    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (// if true then show logout button
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
   )
 }
 
