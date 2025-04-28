499d3340d3b15b39a9be3ad86c6bbd744c97bf35
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-28 19:21:03 +0530

    feat: export additional components from index.js for improved modularity

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -1,7 +1,24 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./Container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Login from "./Login";
+import Button from "./Button";
+import PostCard from "./PostCard";
+import Select from "./Select";
+import Input from "./Input";
 
 export { 
     Header, 
-    Footer 
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Login,
+    Button,
+    PostCard,
+    Select,
+    Input
+    
 };
